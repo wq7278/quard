d7312b277fd1ea446cf737b1b631b2120dbb2df1
unknown <qwang@pci-qi.powercosts.com>
2013-05-14 17:40:19 -0500

    Link to open gl view.

--- TEST/src/com/example/test/DisplayMessageActivity.java
+++ TEST/src/com/example/test/DisplayMessageActivity.java
@@ -1,61 +1,81 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.test;
 
 import java.io.File;
 import java.util.List;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.NavUtils;
 import android.util.FloatMath;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.MotionEvent;
 import de.mindpipe.android.logging.log4j.LogConfigurator;
 
 public class DisplayMessageActivity extends Activity implements SensorEventListener {
 
-	TextView textView = null;
-	ScheduledThreadPoolExecutor stpe = null;
-	SensorManager sm = null;
-	Logger log = null;
-	@SuppressLint("NewApi")
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+    private GLSurfaceView mGLView;
+    private static Logger log = null;
+    
+    private static final float NS2S = 1.0f / 1000000000.0f;
+    private final float[] deltaRotationVector = new float[4];
+    private float timestamp;
+    float [] deltaRotationMatrix = new float[9];
+    
+    float [] axisA = new float [3];
+    float [] axisG = null;//new float [3];
+    
+    long ct =0;
+    float [] oldPos  = new float [3];
+    float [] newPos = new float [3];
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		super.onCreate(savedInstanceState);
-		
-		//confg the log;
-		LogConfigurator logConfigurator = new LogConfigurator();
-        logConfigurator.setFileName(Environment.getExternalStorageDirectory()
-                        + File.separator + "MyApp" + File.separator + "logs"
-                        + File.separator + "TestGyro.txt");
-        logConfigurator.setRootLevel(Level.DEBUG);
-        logConfigurator.setLevel("org.apache", Level.ERROR);
-        logConfigurator.setFilePattern("%d %-5p [%c{2}]-[%L] %m%n");
-        logConfigurator.setMaxFileSize(1024 * 1024 * 15);
-        // logConfigurator.set
-        logConfigurator.setImmediateFlush(false);
-        logConfigurator.setUseLogCatAppender(false);
-        logConfigurator.configure();
+      //confg the log;
+      		LogConfigurator logConfigurator = new LogConfigurator();
+              logConfigurator.setFileName(Environment.getExternalStorageDirectory()
+                              + File.separator + "MyApp" + File.separator + "logs"
+                              + File.separator + "TestGyro.txt");
+              logConfigurator.setRootLevel(Level.INFO);
+              logConfigurator.setLevel("org.apache", Level.ERROR);
+             // logConfigurator.setFilePattern("%d %-5p [%c{2}]-[%L] %m%n");
+              logConfigurator.setFilePattern("%d %-5p  %m%n");
+              logConfigurator.setMaxFileSize(1024 * 1024 * 5);
+              // logConfigurator.set
+              logConfigurator.setImmediateFlush(false);
+              logConfigurator.setUseLogCatAppender(false);
+              logConfigurator.configure();
+              
+              log = Logger.getLogger(DisplayMessageActivity.class);
+              log.info("My Application Created");
         
-        log = Logger.getLogger(DisplayMessageActivity.class);
-        log.info("My Application Created");
-
-        
-		
-		sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-		
-		List<Sensor> typedSensors = sm.getSensorList(Sensor.TYPE_ALL); 
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> typedSensors = sm.getSensorList(Sensor.TYPE_ALL); 
 		System.out.println("List censor: ");
 		for (Sensor s: typedSensors ) {
 			System.out.println(s.getType() +" | " + s.getVendor()+ " | "+s.getVersion());
@@ -63,92 +83,43 @@ public class DisplayMessageActivity extends Activity implements SensorEventListe
 				sm.registerListener(this, s, SensorManager.SENSOR_DELAY_FASTEST); // Rates: SENSOR_DELAY_FASTEST, SENSOR_DELAY_GAME, // SENSOR_DELAY_NORMAL, SENSOR_DELAY_UI
 			}
 		}
-		
-		
-
-	
-		// Get the message from the intent
-		Intent intent = getIntent();
-		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-		// Create the text view
-		textView = new TextView(this);
-		textView.setTextSize(12);
-		textView.setText("ss" + "s" + "\nsdfgsfgsdfz\n");
-
-		// Set the text view as the activity layout
-		setContentView(textView);
-
-		stpe = new ScheduledThreadPoolExecutor(3);
-		
-		//stpe.scheduleWithFixedDelay(this.new RunnableThread(this), 100, 100, TimeUnit.MILLISECONDS);
-	}
-	
-	class RunnableThread implements Runnable {
-       
-		private Activity activity;
-		public RunnableThread(Activity activity){
-			this.activity = activity;
-		}
-        int cnt = 0;
-        @Override
-        public void run() {
-            cnt ++;
-//           
-            activity.runOnUiThread(new Runnable() {
-				
-				@Override
-				public void run() {
-					textView.setText("\n" + cnt + ": " + System.currentTimeMillis()	+ textView.getText());
-					if (textView.getText().length()>1000){
-						textView.setTag(textView.getText().subSequence(0, 1000));
-					}
-				}
-			});
-            
-           // System.out.println(cnt);
-        }
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity
+        mGLView = new MyGLSurfaceView(this, newPos);
+        
+        setContentView(mGLView);
     }
-	
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 
-	public void onDestroy(){//onDestroy
-		super.onDestroy();
-		System.out.println("Destroy is called.");
-		if (stpe != null){
-			stpe.shutdown();
-		}
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // The following call pauses the rendering thread.
+        // If your OpenGL application is memory intensive,
+        // you should consider de-allocating objects that
+        // consume significant memory here.
+        mGLView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The following call resumes a paused rendering thread.
+        // If you de-allocated graphic objects for onPause()
+        // this is a good place to re-allocate them.
+        mGLView.onResume();
+    }
 
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
 		
 	}
 
 	
-	private static final float NS2S = 1.0f / 1000000000.0f;
-    private final float[] deltaRotationVector = new float[4];
-    private float timestamp;
-    float[] deltaRotationMatrix = new float[9];
-    float axisXa = 0;
-    float axisYa = 0;
-    float axisZa = 0;
-    long ct =0;
-    @Override
+	@Override
     public void onSensorChanged(SensorEvent event) {
-    	float axisXg = 0;
-        float axisYg = 0;
-        float axisZg = 0;
-        
+    	
+		axisG = new float [3];
         ct ++;
         if (Sensor.TYPE_GYROSCOPE == event.sensor.getType()){  
     	
@@ -164,9 +135,9 @@ public class DisplayMessageActivity extends Activity implements SensorEventListe
              // Calculate the angular speed of the sample
              float omegaMagnitude = FloatMath.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
              if (omegaMagnitude > 0.03 ) {
-            	 axisXg = axisX;
-            	 axisYg = axisY;
-            	 axisZg = axisZ;
+            	 axisG[0] = axisX;
+            	 axisG[1] = axisY;
+            	 axisG[2] = axisZ;
              }
              // Normalize the rotation vector if it's big enough to get the axis
             
@@ -179,18 +150,98 @@ public class DisplayMessageActivity extends Activity implements SensorEventListe
          // rotationCurrent = rotationCurrent * deltaRotationMatrix;
      
         }else if (Sensor.TYPE_ACCELEROMETER == event.sensor.getType()){
-        	 axisXa = event.values[0];
-             axisYa = event.values[1];
-             axisZa = event.values[2];
+        	 axisA[0] = event.values[0];
+             axisA[1] = event.values[1];
+             axisA[2] = event.values[2];
         }
-        if (ct%100 == 0)textView.setText(ct+"");
+        //if (ct%100 == 0)textView.setText(ct+"");
         
 //       textView.setText("G: \n" + axisXg + ": " + axisYg + ": " + axisZg + "\n"  + 
 //       			 		  "A: \n" + axisXa + ": " + axisYa + ": " + axisZa + "\n");
-        log.debug("G:" + axisXg + ": " + axisYg + ": " + axisZg + "" + "A:" + axisXa + ": " + axisYa + ": " + axisZa + "");
+       // log.debug("G:" + axisG[0] + ": " + axisG[1] + ": " + axisG[2] + "" + "A:" + axisA[0] + ": " + axisA[1] + ": " + axisA[2] + "");
        	// if (textView.getText().length()>1000){
        	//	 textView.setTag(textView.getText().subSequence(0, 1000));
+        
+        //back up the old position vector;
+        {
+	        oldPos[0] = newPos[0];
+	        oldPos[1] = newPos[1];
+	        oldPos[2] = newPos[2];
+        }
+        
+        //assign the new position
+        {
+        	fuse (newPos, oldPos, axisG, axisA);
+        }
+        
        }
-    
 
+	private void fuse(float[] newPos, float[] oldPos, float[] axisG,	float[] axisA) {
+		//Accelo only;
+		newPos[0] = axisA[0];
+		newPos[1] = axisA[1];
+		newPos[2] = axisA[2];
+		
+	}
+}
+
+class MyGLSurfaceView extends GLSurfaceView {
+
+    private final MyGLRenderer mRenderer;
+
+    public MyGLSurfaceView(Context context, float [] pos) {
+        super(context);
+
+        // Create an OpenGL ES 2.0 context.
+        setEGLContextClientVersion(2);
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        mRenderer = new MyGLRenderer();
+        mRenderer.pos = pos;
+        setRenderer(mRenderer);
+
+        // Render the view only when there is a change in the drawing data
+        //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private float mPreviousX;
+    private float mPreviousY;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+
+        float x = e.getX();
+        float y = e.getY();
+
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+
+                float dx = x - mPreviousX;
+                float dy = y - mPreviousY;
+
+                // reverse direction of rotation above the mid-line
+                if (y > getHeight() / 2) {
+                  dx = dx * -1 ;
+                }
+
+                // reverse direction of rotation to left of the mid-line
+                if (x < getWidth() / 2) {
+                  dy = dy * -1 ;
+                }
+
+                mRenderer.mAngle += (dx + dy) * TOUCH_SCALE_FACTOR;  // = 180.0f / 320
+                requestRender();
+        }
+
+        mPreviousX = x;
+        mPreviousY = y;
+        return true;
+    }
+    
+    
+    
 }
